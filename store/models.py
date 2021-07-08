4bcf9c214721486d98276638105fa6fc0f93026b
Inexpli <61479966+Inexpli@users.noreply.github.com>
2021-07-09 00:46:58 +0200

    APP setup, DB conf., media conf. etc.

---
+++ store/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.db.models.fields import SlugField
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/')
+    in_stock = models.BooleanField(default=False)
+    promotion = models.BooleanField(default=False)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    created = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Products'
+        ordering = ('-created',)
+    
+    def __str__(self):
+        return self.title

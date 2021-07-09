fd937e8b10c2480b6489e0233c00086ef9592815
Inexpli <61479966+Inexpli@users.noreply.github.com>
2021-07-09 03:21:35 +0200

    admin.py, db tests

--- core/settings.py
+++ core/settings.py
@@ -116,5 +116,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # MEDIA
 
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Category, Product
 
-admin.site.register(Category, Product)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug':('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','slug','price','in_stock','created','promotion']
+    list_filter = ['in_stock', 'created', 'price']
+    list_editable = ['price', 'in_stock', 'promotion']
+    prepopulated_fields = {'slug':('title',)}
+

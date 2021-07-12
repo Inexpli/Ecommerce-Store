375f73f7ef4b4f5c0313c781ef85587ae8a8cef4
Inexpli <61479966+Inexpli@users.noreply.github.com>
2021-07-12 02:34:23 +0200

    categories, fixes, all

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,7 @@ app_name = 'store'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('category/all/', views.all, name='all'),
+    path('category/<slug:slug>/', views.category, name='category'),
     path('item/<slug:slug>/', views.item, name='item'),
 ]
--- store/views.py
+++ store/views.py
@@ -9,15 +9,24 @@ def categories(request):
 def home(request):
     return render(request, 'store/home.html')
 
-def products(request):
+def all(request):
+    products = Product.objects.all()
     context = {
-        'products': Product.objects.all(),
-        'categories': Category.objects.all()
+        'products': products,
     }
-    return render(request, 'store/products.html', context)
+    return render(request, 'store/all.html', context)
+
+def category(request, slug):
+    category_show = get_object_or_404(Category, slug=slug)
+    products = Product.objects.filter(category_id=category_show)
+    context = {
+        'category_show': category_show,
+        'products': products,
+    }
+    return render(request, 'store/category.html', context)
 
 def item(request, slug):
-    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    product = get_object_or_404(Product, slug=slug)
     context = {
         'product': product,
     }
